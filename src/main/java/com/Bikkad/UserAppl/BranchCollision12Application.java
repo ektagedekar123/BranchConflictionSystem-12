8f8445192898e85b948ba659f7bd21a9c2f33015
ektagedekar123 <gedekarekta123@gmail.com>
2023-01-31 11:28:17 -0800

    Changes by Ekta

---
+++ src/main/java/com/Bikkad/UserAppl/BranchCollision12Application.java
@@ -0,0 +1,13 @@
+package com.Bikkad.UserAppl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchCollision12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchCollision12Application.class, args);
+	}
+
+}
